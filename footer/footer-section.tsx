d006a170742d5872bcd7b56b33d0bb7061f3180c
shajith <shajith@shajith.hack>
2025-06-24 08:56:26 +0530

    Fix Framer Motion translateY property to y in footer component

--- footer/footer-section.tsx
+++ footer/footer-section.tsx
@@ -109,8 +109,8 @@ function AnimatedContainer({ className, delay = 0.1, children }: ViewAnimationPr
 
 	return (
 		<motion.div
-			initial={{ filter: 'blur(4px)', translateY: -8, opacity: 0 }}
-			whileInView={{ filter: 'blur(0px)', translateY: 0, opacity: 1 }}
+			initial={{ filter: 'blur(4px)', y: -8, opacity: 0 }}
+			whileInView={{ filter: 'blur(0px)', y: 0, opacity: 1 }}
 			viewport={{ once: true }}
 			transition={{ delay, duration: 0.8 }}
 			className={className}
